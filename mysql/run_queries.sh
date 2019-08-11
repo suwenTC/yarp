aac1ba0a6da4657eb1365c7fa4c7bea439fdffa5
Suwen Gu <40502186+suwenTC@users.noreply.github.com>
2019-08-11 12:08:23 -0400

    Create run_queries.sh

---
+++ mysql/run_queries.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+bash /root/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN orders AS o ON od.orderNumber = o.orderNumber WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC;"
+
+bash /root/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN orders AS o ON od.orderNumber = o.orderNumber UNION SELECT * from orderdetails AS od Right JOIN orders AS o ON od.orderNumber = o.orderNumber WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC; 
+
+bash /root/run_query.sh "SELECT * from orderdetails AS od CROSS JOIN orders AS o ON od.orderNumber = o.orderNumber WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC;"
+
+bash /root/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN products AS p ON od.productCode = p.productCode WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC"
+
+bash /root/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN products AS p ON od.productCode = p.productCode UNION SELECT * from orderdetails AS od RIGHT JOIN products AS p ON od.productCode = p.productCode WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC;"
+
+bash /root/run_query.sh "SELECT * from orderdetails AS od CROSS JOIN products AS p ON od.productCode = p.productCode WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC"
+
+bash /root/run_query.sh "SELECT * from orderdetails AS od1 CROSS JOIN orderdetails AS od2 ON od1.productCode = od2.productCode WHERE od1.quantityOrdered > 0 ORDER BY od1.quantityOrdered DESC"
