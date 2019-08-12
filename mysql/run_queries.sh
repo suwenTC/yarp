e68c0362dfa50a6e47522873c8646b79cf4a1c13
root <root@node-0.appleseeds.shield-pg0.utah.cloudlab.us>
2019-08-11 22:27:44 -0600

    modified run_quries file

--- mysql/run_queries.sh
+++ mysql/run_queries.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-bash $PWD/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN orders AS o ON od.orderNumber = o.orderNumber WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC;"
+start=$(date +%s)
 
-bash $PWD/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN orders AS o ON od.orderNumber = o.orderNumber UNION SELECT * from orderdetails AS od Right JOIN orders AS o ON od.orderNumber = o.orderNumber WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC;"
+i=1
+while [ $i -le 100 ]
+      do
+	  bash $PWD/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN orders AS o ON od.orderNumber = o.orderNumber WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC;"
 
-bash $PWD/run_query.sh "SELECT * from orderdetails AS od CROSS JOIN orders AS o ON od.orderNumber = o.orderNumber WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC;"
+	  bash $PWD/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN orders AS o ON od.orderNumber = o.orderNumber UNION SELECT * from orderdetails AS od Right JOIN orders AS o ON od.orderNumber = o.orderNumber WHERE od.quantityOrdered > 0;"
 
-bash $PWD/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN products AS p ON od.productCode = p.productCode WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC"
+	  bash $PWD/run_query.sh "SELECT * from orderdetails AS od CROSS JOIN orders AS o ON od.orderNumber = o.orderNumber WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC;"
 
-bash $PWD/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN products AS p ON od.productCode = p.productCode UNION SELECT * from orderdetails AS od RIGHT JOIN products AS p ON od.productCode = p.productCode WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC;"
+	  bash $PWD/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN products AS p ON od.productCode = p.productCode WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC"
 
-bash $PWD/run_query.sh "SELECT * from orderdetails AS od CROSS JOIN products AS p ON od.productCode = p.productCode WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC"
+	  bash $PWD/run_query.sh "SELECT * from orderdetails AS od LEFT JOIN products AS p ON od.productCode = p.productCode UNION SELECT * from orderdetails AS od RIGHT JOIN products AS p ON od.productCode = p.productCode WHERE od.quantityOrdered > 0;"
+	  
+	  bash $PWD/run_query.sh "SELECT * from orderdetails AS od CROSS JOIN products AS p ON od.productCode = p.productCode WHERE od.quantityOrdered > 0 ORDER BY od.quantityOrdered DESC"
 
-bash $PWD/run_query.sh "SELECT * from orderdetails AS od1 CROSS JOIN orderdetails AS od2 ON od1.productCode = od2.productCode WHERE od1.quantityOrdered > 0 ORDER BY od1.quantityOrdered DESC"
+	  bash $PWD/run_query.sh "SELECT * from orderdetails AS od1 CROSS JOIN orderdetails AS od2 ON od1.productCode = od2.productCode WHERE od1.quantityOrdered > 0 ORDER BY od1.quantityOrdered DESC"
+
+	  i=$(( $i + 1 ))
+done
+
+end=$(date +%s)
+touch "time_$1.txt"
+time_cost=$(( end - start ))
+echo $time_cost > "time_$1.txt"
