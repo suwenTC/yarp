b47a9dbe7be07f826d4c1523e04bfa40e501ff4b
root <root@node-0.appleseed.shield-pg0.utah.cloudlab.us>
2019-08-13 10:48:12 -0600

    corrected 2>&1

--- mysql/run_xin1.sh
+++ mysql/run_xin1.sh
@@ -6,9 +6,9 @@ start_date=date
 i=0
 while [ $(( $i - 1 )) -lt $1 ]
 do
-    bash /root/run_queries.sh $i &
+    bash /root/run_queries.sh $i > /dev/null 2>&1 &
 done
-bash /root/run_quries.sh $i
+bash /root/run_quries.sh $i > /dev/null 2>&1
 end=$(date +%s)
 end_date=date
 time_cost=$(( end - start ))
