8fe251bfa6c5805bf4492a50b812726c7ef47d13
root <root@node-0.appleseed.shield-pg0.utah.cloudlab.us>
2019-08-13 19:33:52 -0600

    re-commit

--- mysql/run_xin1.sh
+++ mysql/run_xin1.sh
@@ -6,9 +6,9 @@ start_date=$(date)
 i=0
 while [ $(( $i - 1 )) -lt $1 ]
 do
-    bash /root/run_queries.sh $i > /dev/null 2>&1 &
+    bash /root/run_queries.sh $1 > /dev/null 2>&1 &
 done
-bash /root/run_queries.sh $i > /dev/null 2>&1
+bash /root/run_queries.sh $1 > /dev/null 2>&1
 end=$(date +%s)
 end_date=$(date)
 time_cost=$(( end - start ))
