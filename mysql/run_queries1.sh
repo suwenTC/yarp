58541c507f92e6043e1618e5c2e9dfc3d344d707
root <root@node-0.appleseed.shield-pg0.utah.cloudlab.us>
2019-08-16 22:55:31 -0600

    backup

--- mysql/run_queries1.sh
+++ mysql/run_queries1.sh
@@ -3,20 +3,24 @@
 start=$(date +%s)
 start_date=$(date)
 
-s1=$(date +%s)
-bash $PWD/run_query.sh "SELECT * from title AS t LEFT JOIN title_basics AS tb ON t.titleId = tb.tconst WHERE tb.isAdult = 0 ORDER BY tb.tconst DESC;" > /dev/null 2>&1
-echo $(( $(date +%s) - $s1))
+mysql -uroot -p1234 < /root/sql/imdb.sql
+echo "done loading"
 
-s1=$(date +%s)
-bash $PWD/run_query.sh "SELECT * from title AS t RIGHT JOIN title_basics AS tb ON t.titleId = tb.tconst WHERE t.isOriginalTitle = 0 ORDER BY t.titleId DESC;" > /dev/null 2>&1
-echo $(( $(date +%s) - $s1))
+i=0
+while [ $i -lt 3 ]
+do
+    bash $PWD/run_query.sh "SELECT * from title AS t LEFT JOIN title_basics AS tb ON t.titleId = tb.tconst WHERE tb.isAdult = 0 ORDER BY tb.tconst DESC;" > /dev/null 2>&1
+    
+    bash $PWD/run_query.sh "SELECT * from title AS t RIGHT JOIN title_basics AS tb ON t.titleId = tb.tconst WHERE t.isOriginalTitle = 0 ORDER BY t.titleId DESC;" > /dev/null 2>&1
 
-s1=$(date +%s)
-bash $PWD/run_query.sh "SELECT * from title_basics AS tb RIGHT JOIN title_episode AS te ON tb.tconst = te.tconst WHERE tb.isAdult = 0 ORDER BY tb.tconst ASC;" > /dev/null 2>&1
-echo $(( $(date +%s) - $s1))
+    bash $PWD/run_query.sh "SELECT * from title_basics AS tb RIGHT JOIN title_episode AS te ON tb.tconst = te.tconst WHERE tb.isAdult = 0 ORDER BY tb.tconst ASC;" > /dev/null 2>&1
 
-#end=$(date +%s)
-#end_date=$(date)
-#touch "/root/time_cost/time_$1.txt"
-#time_cost=$(( end - start ))
-#echo "$start_date, $end_date, $time_cost" > "/root/time_cost/time_$1.txt"
+    sleep 1
+    echo $i
+done
+
+end=$(date +%s)
+end_date=$(date)
+touch "/root/time_cost/time_$1.txt"
+time_cost=$(( end - start ))
+echo "$start_date, $end_date, $time_cost" > "/root/time_cost/time_$1.txt"
